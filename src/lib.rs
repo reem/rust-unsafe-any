c402b9118492cf10ef8d949f63fee80c37c7ffe5
Jonathan Reem <jonathan.reem@gmail.com>
2014-08-12 17:33:38 -0700

    (feat) Loosened Any bound to 'static.

--- src/lib.rs
+++ src/lib.rs
@@ -6,8 +6,8 @@
 //! concrete types. These should only be used if you are absolutely certain of the
 //! type of the data in said trait object - there be dragons etc.
 //!
-//! *Heavily* inspired by https://github.com/chris-morgan/anymap
-//! but refactored and re-exported for modularity.
+//! Inspired by https://github.com/chris-morgan/anymap
+//! and the implementation of `std::any::Any`.
 
 use std::any::Any;
 use std::mem;
@@ -17,19 +17,19 @@ use std::raw;
 pub trait UncheckedAnyDowncast<'a> {
     /// Returns a reference to the boxed value, assuming that it is of type `T`. If you
     /// are not _absolutely certain_ of `T` you should _not_ call this!
-    unsafe fn downcast_ref_unchecked<T: Any>(self) -> &'a T;
+    unsafe fn downcast_ref_unchecked<T: 'static>(self) -> &'a T;
 }
 
 /// An extension trait for unchecked downcasting of trait objects to &mut T.
 pub trait UncheckedAnyMutDowncast<'a> {
     /// Returns a mutable reference to the boxed value, assuming that it is of type `T`. If you
     /// are not _absolutely certain_ of `T` you should _not_ call this!
-    unsafe fn downcast_mut_unchecked<T: Any>(self) -> &'a mut T;
+    unsafe fn downcast_mut_unchecked<T: 'static>(self) -> &'a mut T;
 }
 
 impl<'a> UncheckedAnyDowncast<'a> for &'a Any {
     #[inline]
-    unsafe fn downcast_ref_unchecked<T: Any>(self) -> &'a T {
+    unsafe fn downcast_ref_unchecked<T: 'static>(self) -> &'a T {
         // Cast to a trait object, get the data pointer, transmute to T.
         mem::transmute(mem::transmute_copy::<&Any, raw::TraitObject>(&self).data)
     }
@@ -37,7 +37,7 @@ impl<'a> UncheckedAnyDowncast<'a> for &'a Any {
 
 impl<'a> UncheckedAnyMutDowncast<'a> for &'a mut Any {
     #[inline]
-    unsafe fn downcast_mut_unchecked<T: Any>(self) -> &'a mut T {
+    unsafe fn downcast_mut_unchecked<T: 'static>(self) -> &'a mut T {
         // Cast to a trait object, get the data pointer, transmute to T.
         mem::transmute(mem::transmute_copy::<&mut Any, raw::TraitObject>(&self).data)
     }
