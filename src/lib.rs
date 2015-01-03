b11291edb09db4640a223e43d4b0e0456df76bd5
Jonathan Reem <jonathan.reem@gmail.com>
2015-01-03 13:02:50 +0100

    (feat) Rewrite to use new DST inherent implementations and implementations for DST.
    This is a [breaking-change], since all the old traits are removed.
    
    The most straightforward fix is to simply replace all old imports
    with `use uany::UnsafeAnyExt`, but this crate now also exports an `UnsafeAny`
    trait, so that you can store `UnsafeAny` trait objects directly.

--- src/lib.rs
+++ src/lib.rs
@@ -4,76 +4,112 @@
 //! concrete types. These should only be used if you are absolutely certain of the
 //! type of the data in said trait object - there be dragons etc.
 //!
-//! Inspired by https://github.com/chris-morgan/anymap
+//! Originally inspired by https://github.com/chris-morgan/anymap
 //! and the implementation of `std::any::Any`.
 
 use std::any::Any;
 use std::mem;
 use std::raw;
 
-/// An extension trait for unchecked downcasting of trait objects to &T.
-pub trait UncheckedAnyDowncast<'a> {
-    /// Returns a reference to the boxed value, assuming that it is of type `T`. If you
-    /// are not _absolutely certain_ of `T` you should _not_ call this!
-    unsafe fn downcast_ref_unchecked<T: 'static>(self) -> &'a T;
+/// A trait providing unchecked downcasting to its contents when stored
+/// in a trait object.
+pub trait UnsafeAny: Any {}
+impl<T: Any> UnsafeAny for T {}
+
+impl UnsafeAny {
+    /// Returns a reference to the contained value, assuming that it is of type `T`.
+    ///
+    /// ## Warning
+    ///
+    /// If you are not _absolutely certain_ of `T` you should _not_ call this!
+    pub unsafe fn downcast_ref_unchecked<T: 'static>(&self) -> &T {
+        mem::transmute(mem::transmute::<&UnsafeAny, raw::TraitObject>(self).data)
+    }
+
+    /// Returns a mutable reference to the contained value, assuming that it is of type `T`.
+    ///
+    /// ## Warning
+    ///
+    /// If you are not _absolutely certain_ of `T` you should _not_ call this!
+    pub unsafe fn downcast_mut_unchecked<T: 'static>(&mut self) -> &mut T {
+        mem::transmute(mem::transmute::<&mut UnsafeAny, raw::TraitObject>(self).data)
+    }
+
+    /// Returns a the contained value, assuming that it is of type `T`.
+    ///
+    /// ## Warning
+    ///
+    /// If you are not _absolutely certain_ of `T` you should _not_ call this!
+    pub unsafe fn downcast_unchecked<T: 'static>(self: Box<UnsafeAny>) -> Box<T> {
+        mem::transmute(mem::transmute::<Box<UnsafeAny>, raw::TraitObject>(self).data)
+    }
 }
 
-/// An extension trait for unchecked downcasting of trait objects to &mut T.
-pub trait UncheckedAnyMutDowncast<'a> {
-    /// Returns a mutable reference to the boxed value, assuming that it is of type `T`. If you
-    /// are not _absolutely certain_ of `T` you should _not_ call this!
-    unsafe fn downcast_mut_unchecked<T: 'static>(self) -> &'a mut T;
+/// An extension trait for unchecked downcasting of trait objects.
+pub trait UnsafeAnyExt for Sized? {
+    /// Returns a reference to the contained value, assuming that it is of type `T`.
+    ///
+    /// ## Warning
+    ///
+    /// If you are not _absolutely certain_ of `T` you should _not_ call this!
+    unsafe fn downcast_ref_unchecked<T: 'static>(&self) -> &T;
+
+    /// Returns a mutable reference to the contained value, assuming that it is of type `T`.
+    ///
+    /// ## Warning
+    ///
+    /// If you are not _absolutely certain_ of `T` you should _not_ call this!
+    unsafe fn downcast_mut_unchecked<T: 'static>(&mut self) -> &mut T;
+
+    /// Returns a the contained value, assuming that it is of type `T`.
+    ///
+    /// ## Warning
+    ///
+    /// If you are not _absolutely certain_ of `T` you should _not_ call this!
+    unsafe fn downcast_unchecked<T: 'static>(self: Box<Self>) -> Box<T>;
 }
 
-/// An extension for unchecked downcasting of trait objects to Box<T>.
-pub trait UncheckedBoxAnyDowncast {
-    /// Return a box of type Box<T>, assuming the trait object contains a type T. If you are not
-    /// _absolutely certain_ of `T` you should _not_ call this!
-    unsafe fn downcast_unchecked<T: 'static>(self) -> Box<T>;
-}
-
-impl<'a> UncheckedAnyDowncast<'a> for &'a Any {
-    #[inline]
-    unsafe fn downcast_ref_unchecked<T: 'static>(self) -> &'a T {
-        // Cast to a trait object, get the data pointer, transmute to T.
+impl UnsafeAnyExt for Any {
+    unsafe fn downcast_ref_unchecked<T: 'static>(&self) -> &T {
         mem::transmute(mem::transmute::<&Any, raw::TraitObject>(self).data)
     }
-}
 
-impl<'a> UncheckedAnyMutDowncast<'a> for &'a mut Any {
-    #[inline]
-    unsafe fn downcast_mut_unchecked<T: 'static>(self) -> &'a mut T {
-        // Cast to a trait object, get the data pointer, transmute to T.
+    unsafe fn downcast_mut_unchecked<T: 'static>(&mut self) -> &mut T {
         mem::transmute(mem::transmute::<&mut Any, raw::TraitObject>(self).data)
     }
-}
 
-impl UncheckedBoxAnyDowncast for Box<Any> {
-    #[inline]
-    unsafe fn downcast_unchecked<T: 'static>(self) -> Box<T> {
-        let to = mem::transmute::<Box<Any>, raw::TraitObject>(self);
-
-        mem::transmute(to.data)
+    unsafe fn downcast_unchecked<T: 'static>(self: Box<Any>) -> Box<T> {
+        mem::transmute(mem::transmute::<Box<Any>, raw::TraitObject>(self).data)
     }
 }
 
 #[cfg(test)]
 mod test {
-    use super::{UncheckedAnyDowncast, UncheckedAnyMutDowncast, UncheckedBoxAnyDowncast};
+    use super::{UnsafeAny, UnsafeAnyExt};
     use std::any::Any;
 
-    #[test] fn test_simple_downcast() {
+    #[test] fn test_simple_downcast_ext() {
         let a = box 7u as Box<Any>;
         unsafe { assert_eq!(*a.downcast_ref_unchecked::<uint>(), 7u); }
-    }
 
-    #[test] fn test_simply_mut_downcast() {
         let mut a = box 7u as Box<Any>;
         unsafe { assert_eq!(*a.downcast_mut_unchecked::<uint>(), 7u); }
+
+        let mut a = box 7u as Box<Any>;
+        unsafe {
+            *a.downcast_mut_unchecked::<uint>() = 8u;
+            assert_eq!(*a.downcast_mut_unchecked::<uint>(), 8u);
+        }
     }
 
-    #[test] fn test_mut_edit_through_downcast() {
-        let mut a = box 7u as Box<Any>;
+    #[test] fn test_simple_downcast_inherent() {
+        let a = box 7u as Box<UnsafeAny>;
+        unsafe { assert_eq!(*a.downcast_ref_unchecked::<uint>(), 7u); }
+
+        let mut a = box 7u as Box<UnsafeAny>;
+        unsafe { assert_eq!(*a.downcast_mut_unchecked::<uint>(), 7u); }
+
+        let mut a = box 7u as Box<UnsafeAny>;
         unsafe {
             *a.downcast_mut_unchecked::<uint>() = 8u;
             assert_eq!(*a.downcast_mut_unchecked::<uint>(), 8u);
@@ -93,6 +129,14 @@ mod test {
                 self.x.fetch_add(1, Ordering::SeqCst);
             }
         }
+
+        let x = Arc::new(AtomicUint::new(0));
+        let a = box Dropper { x: x.clone() } as Box<UnsafeAny>;
+
+        let dropper = unsafe { a.downcast_unchecked::<Dropper>() };
+        drop(dropper);
+
+        assert_eq!(x.load(Ordering::SeqCst), 1);
 
         let x = Arc::new(AtomicUint::new(0));
         let a = box Dropper { x: x.clone() } as Box<Any>;
